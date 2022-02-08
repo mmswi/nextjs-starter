535f68caed339eab6d561791a1d25551b7dd3c68
Mihai Marinescu <marinescu.mihai88@gmail.com>
2022-02-08 19:20:00 +0200

    feat: removes unnecessary state set

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,9 @@
 import '../styles/styles.scss';
 import type { AppProps } from 'next/app';
 import { wrapper } from 'app/store';
-import { setProfileData } from 'app/store/slices/profile';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-
-MyApp.getInitialProps = wrapper.getInitialAppProps(store => async ({ ctx }) => {
-  console.log(ctx.pathname);
-  store.dispatch(setProfileData('profile name'));
-
-  return {
-    pageProps: {}
-  };
-});
 
 export default wrapper.withRedux(MyApp);
